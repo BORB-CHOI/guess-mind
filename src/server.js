4e41f36847587e1614e6c738fab50d5f581d2bae
BORB_Laptop <qzsec299@naver.com>
2020-10-18 03:20:53 +0900

    SocketIO test

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 2002;
 
@@ -8,6 +9,7 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req,res) => res.render("home"));
@@ -17,4 +19,14 @@ console.log(`✅ Server running: http://localhost:${PORT}`);
 
 const HTTPserver = app.listen(PORT, handleListening);
 
-const io = socketIO(HTTPserver);
+const io = socketIO.listen(HTTPserver); //서버에 socketIO를 올림 그리고 이벤트를 사용하기 위해 변수에 저장
+
+// let sockets = [];
+
+io.on("connection", (socket) => {
+    // sockets.push(socket.id);
+    setTimeout(() => socket.broadcast.emit("hello"), 5000);
+    socket.on("helloServer", () => console.log("The client said hello."));
+}); 
+
+// setInterval(() => console.log(sockets), 1000);
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,4 @@
-hello;
+const socket = io("/");
+
+socket.on("hello", () => console.log("Somebody said hello"));
+setTimeout(() => socket.emit("helloServer"), 4000); 
