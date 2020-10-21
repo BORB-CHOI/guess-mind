5fcfcf6c13290bf9e9b78f30bfde9e2558252684
Your Name <you@example.com>
2020-10-22 02:29:08 +0900

    js transform
    browserify and babelify use

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,11 @@
 import gulp from "gulp"; // gulp를 사용하기 위한
 import sass from "gulp-sass"; // sass에서 css로 변환
 import autoprefixer from "gulp-autoprefixer"; // Webkit 같은 반복작업을 하지 않게
+import browserify from "gulp-browserify"; // 브라우저에는 require 메소드가 정의되어 있지 않지만 Node.js는 정의되어 있습니다.
+// Browserify를 사용하면 Node.js 에서 사용 하는 것과 동일한 방식으로 require 를 사용 하는 코드를 작성할 수 있습니다 .
+import babel from "babelify"; // browserify를 변환하여 require뿐만 아니라 import, export도 사용할 수 있게 해주는 것
 import minifyCSS from "gulp-csso"; // 한줄 코딩화로 소스코드를 mini하게
+import del from "del"; // static 내부 파일을 삭제하기 위한 라이브러리
 
 sass.compiler = require("node-sass");
 
@@ -12,9 +16,17 @@ const paths = {
     // assets/scss/ 에서 확장자가 .scss인 모든 파일
     watch: "assets/scss/**/*.scss",
   },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js/",
+    watch: "assets/js/**/*.js",
+  },
 };
 
 // css 설정
+// arrow로 함수를 만들면 return을 반드시 하게 되어 return을 따로 적지 않아도 되는듯.
+
+const clean = () => del(["src/static"]);
 
 const styles = () => {
   return gulp
@@ -29,13 +41,32 @@ const styles = () => {
     .pipe(gulp.dest(paths.styles.dest));
 };
 
-// ??.scss파일 변화를 체크하고 styles.scss 파일만 컴파일하자
+// JS 설정
+
+const js = () =>
+  gulp
+    .src(paths.js.src)
+    .pipe(
+      browserify({
+        transform: [
+          babel.configure({
+            presets: ["@babel/preset-env"],
+          }),
+        ],
+      })
+    )
+    .pipe(gulp.dest(paths.js.dest));
+
+// **.scss or **.js 파일 변화를 체크하고 styles.scss or main.js 파일만 컴파일하자
 // 왜냐면 어차피 styles.scss파일에서 전부 의존성(import)을 만들어 줄거니까
 
 const watchFile = () => {
   gulp.watch(paths.styles.watch, styles);
+  gulp.watch(paths.js.watch, js);
 };
 
-const dev = gulp.series([styles, watchFile]);
+const dev = gulp.series(clean, styles, js, watchFile);
+
+export const build = gulp.series(clean, styles, js);
 
 export default dev;
--- src/server.js
+++ src/server.js
@@ -12,20 +12,24 @@ app.set("views", join(__dirname, "views"));
 app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
-app.get("/", (req,res) => res.render("home"));
+app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () =>
-console.log(`✅ Server running: http://localhost:${PORT}`);
+  console.log(`✅ Server running: http://localhost:${PORT}`);
 
 const HTTPserver = app.listen(PORT, handleListening);
 
 const io = socketIO.listen(HTTPserver); // 서버에 socketIO를 올림 그리고 이벤트를 사용하기 위해 변수에 저장
 
 io.on("connection", (socket) => {
-    socket.on("newMessage", ({message}) => {
-        socket.broadcast.emit("messageNotif", {
-             message, nickname:socket.nickname || "Anon" });
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon",
     });
-    socket.on("setNickname", ({nickname}) => { socket.nickname = nickname }); 
-    // 소켓은 객체이기 때문에 socket.potato 같이 이론적으로 추가적인 옵션(정보)를 담아 줄 수 있다.
-}); 
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+  // 소켓은 객체이기 때문에 socket.potato 같이 이론적으로 추가적인 옵션(정보)를 담아 줄 수 있다.
+});
