70811dc3fe089226169aa067cd24bae3853be0ef
BORB <qzsec299@naver.com>
2021-01-04 17:32:40 +0900

    moved to dev

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
-import logger from "morgan";
+// import logger from "morgan";
 import socketController from "./socketController";
 import events from "./events";
 
@@ -11,7 +11,7 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req, res) =>
